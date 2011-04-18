4327e2e370ad3b216c592984e4d93ee57ace9bd9
Vernon Lloyd F. Labayog <vernonlloyd@msn.com>
2011-04-18 17:24:14 +0800

    Added User Sign-Up

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 TwitterApp::Application.routes.draw do
   
+
+
   root :to => "pages#home"
 
   match '/contact', :to => 'pages#contact'
   match '/home', :to => 'pages#home'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
-  
+  match '/signup', :to => 'users#new'
+
+  #get "users/new"  
   #get "pages/home"
   #get "pages/contact"
   #get "pages/about"
