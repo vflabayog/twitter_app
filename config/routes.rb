c39e135b2f39532a9cb2c4d9698a67d97b4ae4ed
Vernon Lloyd F. Labayog <vernonlloyd@msn.com>
2011-04-18 16:18:52 +0800

    Links

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 TwitterApp::Application.routes.draw do
-  get "pages/home"
+  
+  root :to => "pages#home"
 
-  get "pages/contact"
-
-  get "pages/about"
+  match '/contact', :to => 'pages#contact'
+  match '/home', :to => 'pages#home'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  
+  #get "pages/home"
+  #get "pages/contact"
+  #get "pages/about"
+  #get "pages/help"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -43,7 +43,7 @@ describe PagesController do
   
   describe "GET 'about'" do
   	it "should be successful" do
-  		get 'contact'
+  		get 'about'
   		response.should be_success
   	end
   	
@@ -57,6 +57,22 @@ describe PagesController do
 		response.body.should_not =~ /<body>\s*<\/body>/
 	end
   end
-  
+
+  describe "GET 'help'" do
+  	it "should be successful" do
+  		get 'help'
+  		response.should be_success
+  	end
+  	
+     it "should have the right title" do
+    	get 'help'
+		response.should have_selector("title", :content => "#{@base_title} | Help")
+	end
+
+	it "should have a non blank body" do
+		get 'help'
+		response.body.should_not =~ /<body>\s*<\/body>/
+	end
+  end  
 
 end
